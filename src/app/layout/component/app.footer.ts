c8941e25126dc64d9588813f96749ba70cf7425c
khanh9x6 <khanh9x6@gmail.com>
2025-08-03 18:00:49 +0700

    update

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
     standalone: true,
     selector: 'app-footer',
     template: `<div class="layout-footer">
-        SAKAI by
-        <a href="https://primeng.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeNG</a>
+       {{ version ? 'Version: ' + version : 'Loading version...' }}
     </div>`
 })
-export class AppFooter {}
+export class AppFooter {
+    version: string = '';
+
+    constructor(private http: HttpClient) {
+        this.http.get<{version: string}>(`assets/app.version.json`).subscribe({
+            next: (data) => this.version = data.version,
+            error: () => this.version = ''
+        });
+    }
+}
